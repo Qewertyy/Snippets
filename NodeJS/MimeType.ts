22b3d692f87323acb0439f9f390336a8de1c34d5
Qewertyy <kh786100@gmail.com>
2024-11-06 23:45:37 +0530

    chore(mimeType): improvements

--- NodeJS/MimeType.ts
+++ NodeJS/MimeType.ts
@@ -6,19 +6,42 @@ type file = {
 //ref = https://github.com/Qewertyy/Icons8-Upscaler/blob/main/src/index.ts#L100
 function guessMimeType(file: Buffer){
   const signature = file.toString("hex", 0, 4);
-    let fileType: file;
-    if (signature === "89504e47") {
-        fileType = {
-            ext: "png",
-            mime: "image/png",
-        };
-    } else if (signature === "ffd8ffe0" || signature === "ffd8ffe1") {
-        fileType = {
-            ext: "jpg",
-            mime: "image/jpeg",
-        };
-    } else {
-        return "Invalid file type";
+  const signatureExtended = file.toString("hex", 0, 12); // Used for WebP and HEIC/HEIF files
+  const sigAscii = file.toString("ascii", 8, 12);
+  const invalid = "Invalid file type";
+  let fileType : file | string;
+  if (signature === "89504e47") {
+      fileType = {
+          ext: "png",
+          mime: "image/png",
+      };
+  } else if (["ffd8ffe0","ffd8ffe1"].includes(signature) || signature.startsWith("ffd8")) { // may have different sub-signatures
+      fileType = {
+          ext: "jpg",
+          mime: "image/jpeg",
+      };
+  } else if (signatureExtended.startsWith("00000018") || signatureExtended.startsWith("0000001c")) {
+    if (["heic","heix"].includes(sigAscii)) {
+      fileType = {
+        ext: "heic",
+        mime: "image/heic",
+      };
+    } else if (["mif1","msf1","heif"].includes(sigAscii)) {
+      fileType = {
+        ext: "heif",
+        mime: "image/heif",
+      };
+    }else{
+      return invalid;
     };
-    return fileType;
+  } else if (signatureExtended.startsWith("52494646") && sigAscii === "WEBP") {
+    // WebP files (RIFF-based)
+    fileType = {
+      ext: "webp",
+      mime: "image/webp",
+    };
+  } else {
+      return invalid;
+  };
+  return fileType;
 };
